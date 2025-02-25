91686cad4bf34f13a458fda23e6bb17c4f7d6081
WizzOFF <maher_off@proton.me>
2025-02-25 16:53:31 +0100

    structure_the_code_with_conditions.c: else

--- structure_the_code_with_conditions.c
+++ structure_the_code_with_conditions.c
@@ -3,11 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-    int age = 20;
-  
-    if (age >= 18)
+    if (age >= 18) // Si l'âge est supérieur ou égal à 18
     {
-      printf ("Vous etes majeur !\n");
+        printf ("Vous etes majeur !");
+    }
+    else // Sinon...
+    {
+        printf ("Ah c'est bete, vous etes mineur !");
     }
      
     return 0;
